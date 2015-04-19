315b0b56f58a250d65e2e86d84908fbdc9b24a24
Per Bernhardt <per@pixelhouse.de>
2015-04-20 00:44:45 +0200

    Korrekturen nach erstem Lesen

--- cd/Implementierung/build_virtualbox.sh
+++ cd/Implementierung/build_virtualbox.sh
@@ -10,13 +10,6 @@ boxes=(
 
 root=$( cd "$( dirname "$0" )" && pwd )
 
-if [ ! -f "$root/machines/ubuntu-14.04.ovf" ]; then
-    wget --no-check-certificate -O "$root/machines/ubuntu-14.04.ovf" "https://www.dropbox.com/s/2e6i2aj9b12xws2/ubuntu-14.04.ovf?dl=1"
-fi
-if [ ! -f "$root/machines/ubuntu-14.04.vmdk" ]; then
-    wget --no-check-certificate -O "$root/machines/ubuntu-14.04.vmdk" "https://www.dropbox.com/s/bka6ph38bmcrsg5/ubuntu-14.04.vmdk?dl=1"
-fi
-
 for box in ${boxes[*]}; do
     cd $root/machines/$box
     vagrant box remove $box
